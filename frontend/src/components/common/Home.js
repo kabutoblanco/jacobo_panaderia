2ac24f305b799c4b09fc5b318a7c4da062592445
kabutoblanco <mdquilindo@unicauca.edu.co>
2020-07-18 02:27:19 -0500

    custom home

--- frontend/src/components/common/Home.js
+++ frontend/src/components/common/Home.js
@@ -18,8 +18,8 @@ export class Home extends Component {
             height={height}
           />
           <Carousel.Caption>
-            <h3>ENVIOS LOCALES</h3>
-            <p>Productos 100% caseros.</p>
+            <h3 style={{color: 'white', textShadow: '0 0 5px #000'}}>HASTA 25% DE DESCUENTO POR COMPRAS ON-LINE</h3>
+            <p style={{color: 'black', textShadow: '0 0 5px #fff'}}>No aplica para productos de tienda.</p>
             <Link to='/favoritos'>
               <Button>ORDENA AHORA</Button>
             </Link>
@@ -34,8 +34,8 @@ export class Home extends Component {
           />
 
           <Carousel.Caption>
-            <h3>ENVIOS LOCALES</h3>
-            <p>Productos 100% caseros.</p>
+            <h3>CUPONES DE DESCUENTO POR COMPRAS ACUMULADAS</h3>
+            <p>Aplica para todos los productos.</p>
             <Link to='/favoritos'>
               <Button>ORDENA AHORA</Button>
             </Link>
@@ -50,8 +50,8 @@ export class Home extends Component {
           />
 
           <Carousel.Caption>
-            <h3>ENVIOS LOCALES</h3>
-            <p>Productos 100% caseros.</p>
+            <h3>ENVIOS A TODA LA CIUDAD POR COMPRAS MAYORES 50 MIL</h3>
+            <p>Menores a este tope solo para barrios en la comunda 2.</p>
             <Link to='/favoritos'>
               <Button>ORDENA AHORA</Button>
             </Link>
