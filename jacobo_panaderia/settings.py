70a0451d07693afeffde6df8a3411413efb9866f
kabutoblanco <mdquilindo@unicauca.edu.co>
2020-08-05 00:38:38 -0500

    as2

--- jacobo_panaderia/settings.py
+++ jacobo_panaderia/settings.py
@@ -121,6 +121,8 @@ LANGUAGE_CODE = "es-co"
 TIME_ZONE = "America/Bogota"
 
 USE_TZ = True
+USE_L10N = True
+USE_I18N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
